8bd4dec770362ea43414e7985c23a1f3e514f32c
Polina Kolesnikova <polina@example.com>
2024-01-26 11:50:28 +0100

    checking comparators

--- src/ComparatorEmpLastName.java
+++ src/ComparatorEmpLastName.java
@@ -3,6 +3,9 @@ import java.util.Comparator;
 public class ComparatorEmpLastName implements Comparator<Employee> {
     @Override
     public int compare(Employee o1, Employee o2) {
-        return o1.getLastName().compareTo(o2.getLastName());
+        if(!o1.getLastName().equals(o2.getLastName())){
+            return o1.getLastName().compareTo(o2.getLastName());
+        }else return o1.getFirstName().compareTo(o2.getFirstName());
+
     }
 }
